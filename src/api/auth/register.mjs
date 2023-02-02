e8207d1bb5dcac0c3cf6d50795210f5eeb66777e
audricdet <audric.det@hotmail.com>
2023-02-02 16:58:13 +0100

    test

--- src/api/auth/register.mjs
+++ src/api/auth/register.mjs
@@ -14,10 +14,11 @@ const register = async (request, response) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Write a SQL query to insert the new user into the database
-    const emailsFromDB = await client.query(`SELECT email FROM users WHERE email = $1`, [email]);
+    
+    const verifyEmail = client.query("SELECT FROM users WHERE email = $1", [email])
 
-    if (emailsFromDB.rows.length === 1) {
-        return res.send({ error: 'This email adress already exist' })
+    if (verifyEmail.rows.length > 0) {
+        return response.json({error : 'Email already exists'})
     }
 
     client.query(
