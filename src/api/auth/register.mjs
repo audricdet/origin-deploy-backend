860f5964a8847d88611dc12dda39fc596001b95e
audricdet <audric.det@hotmail.com>
2023-02-02 17:42:43 +0100

    test

--- src/api/auth/register.mjs
+++ src/api/auth/register.mjs
@@ -5,34 +5,38 @@ const registerRouter = express.Router()
 
 // Define a function for creating a new user
 const register = async (request, response) => {
-    const {
-        email,
-        password
-    } = request.body;
+    const { email, password } = request.body;
+
+    // Vérifiez si l'email ou le mot de passe est manquant
+    if (!email || !password) {
+        return response.status(400).send("L'email ou le mot de passe est manquant");
+    }
+
+    // Vérifiez si l'email existe déjà en effectuant une requête SELECT
+    const existingUser = await client.query(
+        "SELECT * FROM users WHERE email = $1",
+        [email]
+    );
+
+    // Si l'utilisateur existe déjà, renvoyez une réponse "L'utilisateur existe déjà"
+    if (existingUser.rows.length > 0) {
+        return response.status(400).send("L'utilisateur existe déjà");
+    }
 
     // Hash the password using bcrypt
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Write a SQL query to insert the new user into the database
-    
-    const verifyEmail = client.query("SELECT FROM users WHERE email = $1", [email])
-    console.log(verifyEmail)
-    // if (verifyEmail.rows.length > 0) {
-    //     return response.json({error : 'Email already exists'})
-    // }
-
-    client.query(
+    const result = await client.query(
         "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
-        [email, hashedPassword],
-        (error, results) => {
-            if (error) {
-                throw error;
-            }
-            response.status(201).send(`User added with ID: ${results.rows[0].id}`);
-        }
+        [email, hashedPassword]
     );
+
+    response.status(201).send(`User added with ID: ${result.rows[0].id}`);
 };
+
 
 registerRouter.post('/register', register)
 
 export default registerRouter
+
